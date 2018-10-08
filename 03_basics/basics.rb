5e7a645a7aa4e79516542c9ada4d156a768f6760
louismn <louis.martindunord@eemi.com>
2018-10-08 14:50:40 +0200

    avancer

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,30 @@
-# write your code here
+def who_is_bigger(a, b, c)
+    if a == nil || b == nil || c == nil
+        p "nil detected"
+    elsif [a, b, c].max == a
+        p "a is bigger"
+    elsif [a, b, c].max == b
+        p "b is bigger"
+    elsif [a, b, c].max == c
+        p "c is bigger"
+    end
+
+end
+
+
+def reverse_upcase_noLTA(string)
+string.reverse.upcase.delete("L" "T" "A")
+end
+
+def array_42(array)
+ array.include? 42
+end
+
+
+def magic_array(array)
+array.flatten.sort.map{ |n| n * 2 }.delete_if{|n| n % 3 == 0}.uniq.sort
+
+
+end
+
+ puts magic_array([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])
