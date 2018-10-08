e9e7caf20f8237765f6ad45dad16351a3b4ed556
louismn <louis.martindunord@eemi.com>
2018-10-08 12:39:55 +0200

    AJout du fichier calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add(number1, number2)
+	result = number1 + number2
+end
+
+
+def subtract(number1, number2)
+	result = number1 - number2
+end
+
+def sum(array)
+	array.sum
+	end
+
+
+
+def multiply(number1, number2)
+	result = number1 * number2
+end 
+
+def power(number1, number2)
+	result = number1 ** number2
+end
+
+def factorial(number)
+(1..number).inject(1) { |mem, var| mem * var }
+
+end
