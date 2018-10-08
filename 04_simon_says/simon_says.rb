cc1eadb280443903ccb08c19b71b5175d0ce86fe
louismn <louis.martindunord@eemi.com>
2018-10-08 16:55:30 +0200

    Ajout du fichier Simon_says.rb

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,25 @@
-#write your code here
+def echo(value)
+p value
+end
+
+def shout(value)
+p value.upcase
+end
+
+def repeat(word, i=2)
+  word + (" #{word}" * (i-1))   
+end
+
+def start_of_word(word,i)
+	word[0..i-1]
+end
+
+def first_word(string)
+string.split[0]
+end
+
+def titleize(string)
+ no_cap = ["and", "or", "the", "to", "the", "a", "but"]
+i = string.split.map { |word| no_cap.include?(word) ? word : word.capitalize }.join(" ")
+    i = i.capitalize[0] + i[1..-1]
+end
